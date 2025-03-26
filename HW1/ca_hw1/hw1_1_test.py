a1bf498341b28305a66ecbd44cd29003a513fb3a
Bai1026 <1132abc30@gmail.com>
2025-03-27 05:17:32 +0800

    update hw1_2 for passing the testdata under 11 num

--- HW1/ca_hw1/hw1_1_test.py
+++ HW1/ca_hw1/hw1_1_test.py
@@ -1,12 +1,13 @@
 def fact(n):
     if n == 0:
         return 4
-    elif n % 2 == 1:  # 奇數情況
+    elif n % 2 == 1:
         return 4 * fact((n - 1) // 2) + 8 * n + 3
-    else:  # 偶數情況
+    else:
         return 4 * fact((n - 2) // 2) + 8 * n + 3
 
 
-# 測試
 print(fact(7))  # 599
 print(fact(10))  # 655
+print(fact(250))  # 254415
+print(fact(400))  # 940255
--- HW1/ca_hw1/hw1_2.s
+++ HW1/ca_hw1/hw1_2.s
@@ -1,7 +1,8 @@
+
 .data
-nums:   .word 4, 5, 1, 8, 3, 6, 9 ,2    # input sequence
-n:      .word 8                        # sequence length
-dp:     .word 0, 0, 0, 0, 0, 0, 0, 0, 0   # dp array
+nums:   .word 4, 5, 1, 8, 3, 6, 9 ,2, 15, 20, 9    # input sequence
+n:      .word 11                        # sequence length
+dp:     .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0   # dp array
 
 
 .text
@@ -25,6 +26,9 @@ main:
       
     jal LIS         # Jump to LIS algorithm
     
+    li a7, 1        # syscall 1: print integer
+    ecall           # 執行輸出 a0, ecall is syscall -> like output number or string, read input, end seript, memory allocation
+    
     # You should use ret or jalr x1 to jump back after algorithm complete
     # Exit program
     # System id for exit is 10 in Ripes, 93 in GEM5 
@@ -32,4 +36,72 @@ main:
     ecall
 
 LIS:
-    # TODO #
+    li t0, 1
+    mv t1, a2
+    mv t2, a0
+
+init_dp:
+    sw t0, 0(t1)
+    addi t1, t1, 4
+    addi t2, t2, -1
+    bnez t2, init_dp
+
+    li t3, 1              # t3 = i
+outer_loop:
+    bge t3, a0, end_LIS
+
+    li t4, 0              # t4 = j
+inner_loop:
+    bge t4, t3, next_i
+
+    # nums[j]
+    slli t5, t4, 2
+    add t6, a1, t5        # t6 = &nums[j]
+    lw t6, 0(t6)          # t6 = nums[j]
+
+    # nums[i]
+    slli s0, t3, 2
+    add s1, a1, s0        # s1 = &nums[i]
+    lw s1, 0(s1)          # s1 = nums[i]
+
+    bge t6, s1, skip_update
+
+    # dp[j] + 1
+    add s2, a2, t5        # s2 = &dp[j]
+    lw s2, 0(s2)
+    addi s2, s2, 1
+
+    # dp[i]
+    add s3, a2, s0        # s3 = &dp[i]
+    lw t5, 0(s3)          # t5 = dp[i]
+
+    bge t5, s2, skip_update
+    sw s2, 0(s3)          # dp[i] = s2
+
+skip_update:
+    addi t4, t4, 1
+    j inner_loop
+
+next_i:
+    addi t3, t3, 1
+    j outer_loop
+
+end_LIS:
+    li t3, 0              # i
+    li t4, 1              # max
+
+find_max:
+    bge t3, a0, return_result
+    slli t5, t3, 2
+    add t5, a2, t5
+    lw t6, 0(t5)
+    bge t4, t6, skip_max_update
+    mv t4, t6
+
+skip_max_update:
+    addi t3, t3, 1
+    j find_max
+
+return_result:
+    mv a0, t4
+    ret
